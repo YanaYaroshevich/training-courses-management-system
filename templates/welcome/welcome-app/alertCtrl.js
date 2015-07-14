9a4d1d8d0e40024c43560c7c802a32286971c7f6
Yana <yaroshevich.yana@gmail.com>
2015-07-14 13:02:56 +0300

    TCMS-21: angular notifications

--- templates/welcome/welcome-app/alertCtrl.js
+++ templates/welcome/welcome-app/alertCtrl.js
@@ -2,13 +2,18 @@ var alertCtrl = angular.module('alertCtrl', []);
 
 alertCtrl.controller('alertCtrl', ['$scope', '$sce', function($scope, $sce){
 	$scope.alerts = [
-    { type: 'info', msg: 'Your training ##### is aproved', time: '2016-07-21 18:20', link: 'http://google.com', trainingName: 'Lala'},
-    { type: 'success', msg: 'Well done! You successfully read this important alert ##### message.', time: '', link: '#', trainingName: 'uuu'}
+    { type: 'info', msg: 'Your training $$trainingName$$ is aproved', time: '2016-07-21 18:20', link: '../training.html', trainingName: 'Assembler: best-practice'},
+    { type: 'danger', msg: 'You have got training $$trainingName$$ in an hour!', time: '2016-07-21 15:10', link: '../training.html', trainingName: 'C++'},
+    { type: 'success', msg: 'You have got training $$trainingName$$ in a day!', time: '2016-07-21 12:07', link: '../training.html', trainingName: 'Scala and Go'},
+    { type: 'warning', msg: 'User $$userName$$ sent you new $$trainingName$$ to aprove.', time: '2016-07-21 12:07', link: '../training.html', trainingName: 'Scala and Go', userName: 'Yana Yaroshevich', userLink: '../user.html'}
   ];
 
   $scope.messages = $scope.alerts.map(function(alert) {
   	var s = alert.msg;
-  	s = $sce.trustAsHtml(s.replace('#####', '<a href="' + alert.link + '">' + alert.trainingName + '</a>'));
+  	if (alert.type == 'warning'){
+  		s = s.replace('$$userName$$', '<a href="' + alert.userLink + '"class="alert-link">' + alert.userName + '</a>');
+  	}
+  	s = $sce.trustAsHtml(s.replace('$$trainingName$$', '<a href="' + alert.link + '"class="alert-link">' + alert.trainingName + '</a>'));
   	return s;
   });
 
