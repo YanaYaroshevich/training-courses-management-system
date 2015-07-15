f955741004e4f3244b984d609df27500949b8619
Yana <yaroshevich.yana@gmail.com>
2015-07-15 14:57:32 +0300

    TCMS-21: newTraining.html pagination

--- templates/newTraining/newTraining-app/paginationCtrl.js
+++ templates/newTraining/newTraining-app/paginationCtrl.js
@@ -1,17 +1,12 @@
 var paginationCtrl = angular.module('paginationCtrl', []);
-paginationCtrl.controller('paginationCtrl', function ($scope, $log) {
-  $scope.totalItems = 64;
-  $scope.currentPage = 1;
 
+paginationCtrl.controller('paginationCtrl', function ($scope, $log) {
+  $scope.totalItems = 30;
+  $scope.currentPage = 1;
+  $scope.itemsPerPage = 1;
+
+  //window.scope = $scope;
   $scope.setPage = function (pageNo) {
     $scope.currentPage = pageNo;
   };
-
-  $scope.pageChanged = function() {
-    $log.log('Page changed to: ' + $scope.currentPage);
-  };
-
-  $scope.maxSize = 5;
-  $scope.bigTotalItems = 175;
-  $scope.bigCurrentPage = 1;
 });
