bc1b5f56a2b13cb0493a1874a99766fbb3b242c2
slawiko <shchaurouski.slava@gmail.com>
2015-07-16 16:26:14 +0300

    TCMS-27: Delete event icon with action added

--- templates/README.md
+++ templates/README.md
@@ -1,5 +1,15 @@
-Colors: 
+##Colors: 
+
     Header: #1A394C
     Menu:hover: #285069
     Button:hover: #112c3c
     Shadow: #0f2736
+    
+##Colors of events in calendar:
+
+    inverse: black
+    important: red
+    warning: yellow
+    info: blue
+    succes: green
+    special: purple
--- templates/pages/calendar/calendar-app/main-ctrl.js
+++ templates/pages/calendar/calendar-app/main-ctrl.js
@@ -9,22 +9,22 @@ mainCtrl.controller('mainCtrl', ['$scope', /*'$modal',*/ function($scope, /*$mod
         {
             title: 'Front-end',
             type: 'warning',
-            startsAt: new Date(2015,6,20,14),
-            endsAt: new Date(2015,6,25,16),
+            startsAt: new Date(2015,6,1,14),
+            endsAt: new Date(2015,6,5,16),
             draggable: true,
             resizable: true
         }, {
             title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
             type: 'info',
-            startsAt: new Date(2015,6,21,13),
-            endsAt: new Date(2015,6,26,14),
+            startsAt: new Date(2015,6,2,13),
+            endsAt: new Date(2015,6,6,14),
             draggable: true,
             resizable: true
         }, {
             title: 'This is a really long event title that occurs on every year',
             type: 'important',
-            startsAt: new Date(2015,6,22,12),
-            endsAt: new Date(2015,6,27,13),
+            startsAt: new Date(2015,6,3,12),
+            endsAt: new Date(2015,6,7,13),
             recursOn: 'year',
             draggable: true,
             resizable: true
@@ -41,23 +41,23 @@ mainCtrl.controller('mainCtrl', ['$scope', /*'$modal',*/ function($scope, /*$mod
             },
             controllerAs: 'vm'
         });
-    }
+    }*/
     
     vm.eventClicked = function(event) {
-        showModal('Clicked', event);
+        //showModal('Clicked', event);
     };
 
     vm.eventEdited = function(event) {
-        showModal('Edited', event);
+        //showModal('Edited', event);
     };
 
     vm.eventDeleted = function(event) {
-        showModal('Deleted', event);
+        vm.events.splice(event, 1);
     };
 
     vm.eventTimesChanged = function(event) {
-        showModal('Dropped or resized', event);
-    };*/
+        //showModal('Dropped or resized', event);
+    };
 
     vm.toggle = function($event, field, event) {
         $event.preventDefault();
