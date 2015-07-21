acdf817e9789e76894ba676f19619bb16c444312
RomashkoVictor <phantom.rvr@gmail.com>
2015-07-21 17:28:27 +0300

    TCMS-45: Add annotation

--- src/main/java/org/exadel/training/model/Schedule.java
+++ src/main/java/org/exadel/training/model/Schedule.java
@@ -1,9 +1,17 @@
 package org.exadel.training.model;
 
-import java.util.Date;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Date;
+@EnableScheduling
+@EnableAsync
 public class Schedule {
-    public void xmlCronTask() {
+    @Async
+    @Scheduled(cron="*/5 * * * * *")
+    public void scheduleTask() {
         System.out.println(new Date() + " This task runs in a cron schedule by xml configuration");
     }
 }
