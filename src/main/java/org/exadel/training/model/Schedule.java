9fa10110bd9686c8d7d6f7431da55b8c1485f39e
RomashkoVictor <phantom.rvr@gmail.com>
2015-07-21 13:21:12 +0300

    TCMS-45: Create schedule example. work like timer

---
+++ src/main/java/org/exadel/training/model/Schedule.java
@@ -0,0 +1,9 @@
+package org.exadel.training.model;
+
+import java.util.Date;
+
+public class Schedule {
+    public void xmlCronTask() {
+        System.out.println(new Date() + " This task runs in a cron schedule by xml configuration");
+    }
+}
